1881df2f9a3f096b83f7f81faa6322e35d474a6a
Luke Mason <s3630120@gpuserver1head.csit.local>
2023-11-07 13:11:04 +1100

    Added model

--- run_experiment.sh
+++ run_experiment.sh
@@ -12,6 +12,6 @@ number=$1
 # Loop from 0 to 9 (inclusive) and call the Python script
 for i in {0..9}; do
     config="experiments/dstl_ex${number}.json"
-    echo "Calling srun --mem=50G --cpus-per-task=8 --gres=gpu:1 python3 dstl_train.py --config $config --cl $i"
-    srun --mem=50G --cpus-per-task=8 --gres=gpu:1 python3 dstl_train.py --config "$config" --cl "$i"
+    echo "Calling srun --mem=50G --cpus-per-task=8 --gres=gpu:2 python3 dstl_train.py --config $config --cl $i"
+    srun --mem=50G --cpus-per-task=8 --gres=gpu:2 python3 dstl_train.py --config "$config" --cl "$i"
 done
